43b22814991baa016d1e14de9c13c2634f7eab0d
Daniele Marques <133058622+D4nnyXM@users.noreply.github.com>
2024-04-22 22:25:50 -0300

    Add files via upload

---
+++ AULAEXTRA/script.js
@@ -0,0 +1,26 @@
+let A = 10, B = 30, total;
+if (A>B) {
+     total = 2*A;
+}else{
+     total = A + B;
+};
+alert(total); 
+
+
+// variavel divisivel somente por ela mesma ou 1 
+
+//ativ 2 testar valor de variavel =7 se ela é um numero primo
+
+
+
+
+
+//3 variavel recebe valor de uma venda, se a venda for MAIOR  
+//que 1.000,00, aplicar comissao de 15%, se nao, 10%
+
+
+//4 crie 3 variaveis com diferentes valores a,b,c diga se a é o 
+// maior valor, o menor ou do meio 
+
+
+ 
